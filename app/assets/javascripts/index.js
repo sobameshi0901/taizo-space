0268a6929811acc41df79d436fdaeb32bc4d35cd
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-08-17 15:54:21 +0900

    add group user registration and remove function "

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,44 +1,65 @@
-$(function (){
+$(document).on('turbolinks:load', function() {
 
-  function appendUser(user) {
-    var search_list = $('#user-search-result');
+  $(function() {
 
-    var html = `<div class="chat-group-user clearfix">
-  <p class="chat-group-user__name">${user.name}</p>
-  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-</div>`
-  search_list.append(html);
-  }
+    function appendUser(user) {
+      var search_list = $('#user-search-result');
 
-  function appendNoUser(user) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-  <p class='chat-group-user__name'>${user}</p>
-</div>`
-  }
+      var html = `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${user.name}</p>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+  </div>`
+    search_list.append(html);
+    }
 
-  $('#user-search-field').on('keyup', function() {
-    $('#user-search-result').empty();
-    var input = $('#user-search-field').val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(users) {
-      console.log(users);
-      $('.user-search-result').empty();
-      if (users.length !== 0) {
-        users.forEach(function(user) {
-          appendUser(user);
-        });
-      }
-        else {
-          appendNoUser('一致するユーザーは存在しません');
-        }
+    function appendNoUser(user) {
+      var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    <p class='chat-group-user__name'>${user}</p>
+  </div>`
+    }
+
+    function appendUserGroup(userId, userName) {
+      var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${userId}'>
+    <input name='group[user_ids][]' type='hidden' value='${userId}'>
+    <p class='chat-group-user__name'>${userName}</p>
+    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+  </div>`
+    $('#chat-group-users').append(html)
+    }
+
+    $('#user-search-field').on('keyup', function() {
+      $('#user-search-result').empty();
+      var input = $('#user-search-field').val();
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
       })
+      .done(function(users) {
+        console.log(users);
+        $('.user-search-result').empty();
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            appendUser(user);
+          });
+        }
+          else {
+            appendNoUser('一致するユーザーは存在しません');
+          }
+        })
       .fail(function() {
         alert('ユーザー検索に失敗しました');
       })
     });
+
+    $(document).on('click', '.chat-group-user__btn--add', function() {
+      $(this).parent().remove();
+      appendUserGroup($(this).data('user-id'), $(this).data('user-name'));
+    })
+
+    $(document).on('click', '.chat-group-user__btn--remove', function() {
+      $(this).parent().remove();
+    })
   });
+});
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,50 +1,52 @@
-$(function() {
-  function buildHTML(message){
-    var content = message.content ? message.content : ""
-    var image = message.image ?  `<img src="${message.image}">` : ""
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    function buildHTML(message){
+      var content = message.content ? message.content : ""
+      var image = message.image ?  `<img src="${message.image}">` : ""
 
-    var html = `
-      <div class="post">
-        <ul class="post__detail claerfix">
-          <li class="post__detail__user">
-            ${message.user_name}
-          </li>
-          <li class="post__detail__time">
-            ${message.created_at}
-          </li>
-        </ul>
-        <div class="post__text">
-          ${content}
-        </div>
-        ${image}
-      </div>`
-      return html;
-  }
+      var html = `
+        <div class="post">
+          <ul class="post__detail claerfix">
+            <li class="post__detail__user">
+              ${message.user_name}
+            </li>
+            <li class="post__detail__time">
+              ${message.created_at}
+            </li>
+          </ul>
+          <div class="post__text">
+            ${content}
+          </div>
+          ${image}
+        </div>`
+        return html;
+    }
 
-  $('#new_message').on('submit',function(e) {    
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $('.message__submit').removeAttr('data-disable-with')
+    $('#new_message').on('submit',function(e) {    
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $('.message__submit').removeAttr('data-disable-with')
 
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data) {
+        var html = buildHTML(data)
+        $('.chat-main__body').append(html);
+        $('#message_content').val('');
+        $('#message_image').val('');
+        $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
+
+      })
+      .fail(function() {
+        alert('error');
+      })
     })
-    .done(function(data) {
-      var html = buildHTML(data)
-      $('.chat-main__body').append(html);
-      $('#message_content').val('');
-      $('#message_image').val('');
-      $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
-
-    })
-    .fail(function() {
-      alert('error');
-    })
-  })
+  });
 });
