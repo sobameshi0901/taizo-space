9598fd70d57a2959cf59ea246b76682a6a39ba99
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-08-18 22:24:40 +0900

    revise user_search_function

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -30,7 +30,6 @@ $(document).on('turbolinks:load', function() {
       dataType: 'json'
     })
     .done(function(users) {
-      console.log(users);
       $('.user-search-result').empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
---
+++ app/views/messages/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array! @new_messages do |message|
+  json.content message.content if message.content
+  json.image message.image.url if message.image
+  json.user_name message.user.name
+  json.created_at message.created_at
+  json.id message.id
+end
