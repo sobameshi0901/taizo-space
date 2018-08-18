1cb64cc9967c57ce0d53a39396f91e1899245764
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-08-18 22:23:03 +0900

    define new_message_check function on message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,8 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 
+  def self.new_messages(message_id, group_id)
+    where("group_id = ? and id > ?", group_id, message_id)
+  end
+
 end
